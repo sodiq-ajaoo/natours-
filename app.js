8cf3ad098eb23e731e3b04381e3480052fd9c7f5
sodiq-ajaoo <sodiqhammedajao123@gmail.com>
2026-07-16 01:12:44 -0700

    implemented cors

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utit/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -30,6 +31,10 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // app.use(express.static(path.join(__dirname, 'public')));
+//implement cors
+app.use(cors());
+app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors());
 
 // const app = express();
 // middlewares
